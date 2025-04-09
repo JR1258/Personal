26a7fc38c7f6c14d9a2f1f74e9ca2fce4676301c
JR1258 <113603723+JR1258@users.noreply.github.com>
2025-04-09 12:48:50 +0200

    push all

---
+++ ArrayList/CodeStepbyStep/SwitchPairs.java
@@ -0,0 +1,12 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class switchPairs {
+//https://www.codestepbystep.com/r/problem/view/java/collections/arraylist/switchPairs?problemsetid=4327 
+
+
+
+}
+
+
