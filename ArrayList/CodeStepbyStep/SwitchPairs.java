3a757126cf08ecc5e5d0f8c23611d6b96aa38095
JR1258 <113603723+JR1258@users.noreply.github.com>
2025-04-10 13:40:54 +0200

    SwitchPairs.2

--- ArrayList/CodeStepbyStep/SwitchPairs.java
+++ ArrayList/CodeStepbyStep/SwitchPairs.java
@@ -13,8 +13,24 @@ for (int i = 0; i < list.size() - 1; i += 2) {
    
 
 
+
 }
 }    return list;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> list = new ArrayList<>();
+        System.out.println("Enter numbers (0 to stop):");
+        while (true) {
+            int num = scanner.nextInt();
+            if (num == 0) {
+                break;
+            }
+            list.add(num);
+        }
+        System.out.println("Original list: " + list);
+        ArrayList<Integer> switchedList = switchPairs(list);
+        System.out.println("Switched pairs: " + switchedList);
+    }
 
 
 
