a98a9b95e2ed444e6be7c43a8908aeda022032cf
JR1258 <113603723+JR1258@users.noreply.github.com>
2025-04-10 13:36:54 +0200

    SwitchPairs

--- ArrayList/CodeStepbyStep/SwitchPairs.java
+++ ArrayList/CodeStepbyStep/SwitchPairs.java
@@ -6,7 +6,16 @@ public class switchPairs {
 //https://www.codestepbystep.com/r/problem/view/java/collections/arraylist/switchPairs?problemsetid=4327 
 
 
+for (int i = 0; i < list.size() - 1; i += 2) {
+    int temp = list.get(i);
+    list.set(i, list.get(i + 1));
+    list.set(i + 1, temp);
+   
+
 
 }
+}    return list;
+
+
 
 
