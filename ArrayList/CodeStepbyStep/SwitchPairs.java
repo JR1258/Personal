c0e464a4c7582bfb103c782b0c0186d8911a721d
JR1258 <113603723+JR1258@users.noreply.github.com>
2025-04-10 13:57:56 +0200

    SwitchPairs finish

--- ArrayList/CodeStepbyStep/SwitchPairs.java
+++ ArrayList/CodeStepbyStep/SwitchPairs.java
@@ -1,37 +1,30 @@
 
 import java.util.ArrayList;
-import java.util.Scanner;
-
-public class switchPairs {
 //https://www.codestepbystep.com/r/problem/view/java/collections/arraylist/switchPairs?problemsetid=4327 
 
 
-for (int i = 0; i < list.size() - 1; i += 2) {
-    int temp = list.get(i);
-    list.set(i, list.get(i + 1));
-    list.set(i + 1, temp);
-   
+public static void switchPairs(ArrayList<String> list) {
 
+    for (int i = 0; i < list.size() - 1; i += 2) {
+                    String temp = list.get(i);
+                    list.set(i, list.get(i + 1));
+                    list.set(i + 1, temp);
 
-
-}
-}    return list;
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> list = new ArrayList<>();
-        System.out.println("Enter numbers (0 to stop):");
-        while (true) {
-            int num = scanner.nextInt();
-            if (num == 0) {
-                break;
-            }
-            list.add(num);
-        }
-        System.out.println("Original list: " + list);
-        ArrayList<Integer> switchedList = switchPairs(list);
-        System.out.println("Switched pairs: " + switchedList);
+        }   
     }
 
 
+// Test the switchPairs method
+public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("A");
+    list.add("B");
+    list.add("C");
+    list.add("D");
+    list.add("E");
 
-
+    System.out.println("Before switching: " + list);
+    switchPairs(list);
+    System.out.println("After switching: " + list);
+}
+// Output:
